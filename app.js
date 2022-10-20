f847a703079cadf8b0a9988d45e29c1deb2b6986
Shmaither <shmaither@gmail.com>
2022-10-19 19:25:56 -0600

    feature: psyduck eyes rotation on mousemove

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const pokeAPIBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
 const game = document.getElementById("game");
 
+const anchor = document.querySelector(".psyduck");
+const rekt = anchor.getBoundingClientRect();
+const anchorX = rekt.left + rekt.width / 2;
+const anchorY = rekt.top + rekt.height / 2;
+
+document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+
+    const eyes = document.querySelectorAll(".eye");
+    eyes.forEach((eye) => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+        anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+    });
+});
+
+const angle = (cx, cy, ex, ey) => {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = (rad * 180) / Math.PI;
+    return deg;
+};
+
 let isPaused = false;
 let firstPick;
 let matches = 0;
